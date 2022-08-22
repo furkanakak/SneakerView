3d6c7ac607c3c22c6cee986954d306bf81e043eb
furkanak <furkanakalin66ff@gmail.com>
2022-08-22 14:35:42 +0300

    bug fixed

--- app/src/main/java/com/furkan/sneaker/MainActivity.kt
+++ app/src/main/java/com/furkan/sneaker/MainActivity.kt
@@ -3,38 +3,137 @@ package com.furkan.sneaker
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.furkan.sneaker.databinding.ActivityMainBinding
 import com.furkan.sneaker_view.Sneaker
 import com.furkan.sneaker_view.SneakerListener
 import com.furkan.sneaker_view.SneakerType
 
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-     Sneaker.Builder()
-            .setActivity(this)
-            .setType(SneakerType.Error)
-            .setTitle("deneme")
-            .setSneakerDuration(3000)
-            .setContent("denemecontent")
-            .setIconVisible(true)
-            .setAnimateDuration(1000)
-            .setAnimationRepeatCount(3)
-            .setArrowIcon(true)
-            .setButtonName("")
-            .setAutoHide(false)
-            .setArrowIcon(true)
-            .setAnimation(true)
-            .setButtonIconListener(object : SneakerListener{
-                override fun onClickListener() {
-                   Log.v("buttonClick","buttonClick")
-                }
-            })
-          .build()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.success.setOnClickListener {
+            Log.v("sneaker","success")
+            Sneaker.Builder()
+                .setActivity(this) /* optionally, these -> .setFragment() .setViewGroup() */
+                .setType(SneakerType.Success)
+                .setTitle("Success")
+                .setSneakerDuration(3000)
+                .setContent("Your transaction was successful")
+                .setIconVisible(true)
+                .setAnimateDuration(1000)
+                .setAnimationRepeatCount(3)
+                .setArrowIcon(true)
+                .setButtonName("Action")
+                .setAutoHide(false)
+                .setArrowIcon(true)
+                .setAnimation(true)
+                .setButtonIconListener(object : SneakerListener {
+                    override fun onClickListener() {
+                        Log.v("buttonClick","buttonClick")
+                    }
+                })
+                .build()
+        }
+        binding.info.setOnClickListener {
+            Log.v("sneaker","info")
+            Sneaker.Builder()
+                .setActivity(this)
+                .setType(SneakerType.Info)
+                .setTitle("Info")
+                .setSneakerDuration(3000)
+                .setContent("Lorem Ipsum is simply dummy")
+                .setIconVisible(true)
+                .setAnimateDuration(1000)
+                .setAnimationRepeatCount(3)
+                .setArrowIcon(true)
+                .setButtonName("Action")
+                .setAutoHide(false)
+                .setArrowIcon(true)
+                .setAnimation(true)
+                .setButtonIconListener(object : SneakerListener {
+                    override fun onClickListener() {
+                        Log.v("buttonClick","buttonClick")
+                    }
+                })
+                .build()
+
+        }
+        binding.infoError.setOnClickListener {
+            Log.v("sneaker","infoError")
+            Sneaker.Builder()
+                .setActivity(this)
+                .setType(SneakerType.InfoError)
+                .setTitle("InfoError")
+                .setSneakerDuration(3000)
+                .setContent("It has survived not only five")
+                .setIconVisible(true)
+                .setAnimateDuration(1000)
+                .setAnimationRepeatCount(3)
+                .setArrowIcon(true)
+                .setButtonName("Action")
+                .setAutoHide(false)
+                .setArrowIcon(true)
+                .setAnimation(true)
+                .setButtonIconListener(object : SneakerListener {
+                    override fun onClickListener() {
+                        Log.v("buttonClick","buttonClick")
+                    }
+                })
+                .build()
+        }
+        binding.warning.setOnClickListener {
+            Log.v("sneaker","warning")
+            Sneaker.Builder()
+                .setActivity(this)
+                .setType(SneakerType.Warning)
+                .setTitle("Warning")
+                .setSneakerDuration(3000)
+                .setContent("Lorem Ipsum passages")
+                .setIconVisible(true)
+                .setAnimateDuration(1000)
+                .setAnimationRepeatCount(3)
+                .setArrowIcon(true)
+                .setButtonName("Action")
+                .setAutoHide(false)
+                .setArrowIcon(true)
+                .setAnimation(true)
+                .setButtonIconListener(object : SneakerListener {
+                    override fun onClickListener() {
+                        Log.v("buttonClick","buttonClick")
+                    }
+                })
+                .build()
+        }
+        binding.error.setOnClickListener {
+            Log.v("sneaker","error")
+            Sneaker.Builder()
+                .setActivity(this)
+                .setType(SneakerType.Error)
+                .setTitle("Error")
+                .setSneakerDuration(3000)
+                .setContent("Your transaction could not be")
+                .setIconVisible(true)
+                .setAnimateDuration(1000)
+                .setAnimationRepeatCount(3)
+                .setArrowIcon(true)
+                .setButtonName("Action")
+                .setAutoHide(false)
+                .setArrowIcon(true)
+                .setAnimation(true)
+                .setButtonIconListener(object : SneakerListener {
+                    override fun onClickListener() {
+                        Log.v("buttonClick","buttonClick")
+                    }
+                })
+                .build()
+        }
 
     }
 }
